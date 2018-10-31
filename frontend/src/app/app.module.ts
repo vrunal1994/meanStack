c6089078ee4ec14fc28c477d2a8088f79d960e83
Vrunal Mhatre <vrunal1994@gmail.com>
2018-10-31 19:49:56 -0400

    SecondDayCommit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {RouterModule , Routes} from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './components/list/list.component';
+import { EditComponent } from './components/edit/edit.component';
+import { CreateComponent } from './components/create/create.component';
+
+//Array of type 'Routes' for configuring routing
+const routes : Routes=[
+  { path : 'create' , component:CreateComponent },
+  { path : 'edit/:id' , component:EditComponent },
+  { path : 'list' , component:ListComponent },
+  { path : '' , redirectTo:'list', pathMatch: 'full' },
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    EditComponent,
+    CreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)  //Activating configuration for routes
   ],
   providers: [],
   bootstrap: [AppComponent]
